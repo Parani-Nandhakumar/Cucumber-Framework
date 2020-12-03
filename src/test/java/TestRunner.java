14a9777f201c56d138af19f1ff4a441622b32103
Parani-Nandhakumar <parani777@gmail.com>
2020-12-03 21:20:00 +0530

    Added Cucumber Reporting

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,12 +1,43 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resource"},
-        glue = "StepDefinitions"
+        glue = {"StepDefinitions"},
+        plugin = {"pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json"
+        }
 )
 
-public class TestRunner {
+public class TestRunner{
+        public TestNGCucumberRunner testNGCucumberRunner;
+
+        @BeforeClass(alwaysRun = true)
+        public void setUpClass() {
+                testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+        }
+
+        @Test(
+                groups = {"cucumber"},
+                description = "Runs Cucumber Scenarios",
+                dataProvider = "scenarios"
+        )
+        public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+                this.testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
+        }
+
+        @DataProvider
+        public Object[][] scenarios() {
+                return this.testNGCucumberRunner.provideScenarios();
+        }
+
+        @AfterClass(
+                alwaysRun = true
+        )
+        public void tearDownClass() {
+                testNGCucumberRunner.finish();
+        }
 }
